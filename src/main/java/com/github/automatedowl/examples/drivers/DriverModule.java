d4cac21ed7eeb3271c5441f87b64871ce57fd5c7
rishikhagupta <84220626+rishikhagupta@users.noreply.github.com>
2021-09-09 17:09:16 +0100

    Update DriverModule.java

--- src/main/java/com/github/automatedowl/examples/drivers/DriverModule.java
+++ src/main/java/com/github/automatedowl/examples/drivers/DriverModule.java
@@ -4,6 +4,7 @@ import com.google.inject.AbstractModule;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 /**
  * Guice configuration module for WebDriver object.
@@ -16,8 +17,16 @@ public class DriverModule extends AbstractModule {
         // Set Chromedriver executable path.
         setChromedriverPath();
 
+ChromeOptions chromeOptions = new ChromeOptions();
+chromeOptions.addArguments("--headless");
+//chromeOptions.addArguments("--remote-debugging-port=9222");
+chromeOptions.addArguments("--disable-gpu");
+chromeOptions.addArguments("--no-sandbox");
+
+        
         // Bind WebDriver object to ChromeDriver.
-        bind(WebDriver.class).toInstance(new ChromeDriver());
+//        bind(WebDriver.class).toInstance(new ChromeDriver());
+        bind(WebDriver.class).toInstance(new ChromeDriver(chromeOptions));
     }
 
     private void setChromedriverPath() {
